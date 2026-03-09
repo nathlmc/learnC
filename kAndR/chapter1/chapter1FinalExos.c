23d5430843bf568a986747d25900e785331cd2bd
nathlmc <nlmcfr@outlook.com>
2026-03-09 01:44:07 +0000

    Finished 2 final exos. Leave rest for later

--- kAndR/chapter1/chapter1FinalExos.c
+++ kAndR/chapter1/chapter1FinalExos.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #define MAXLINE 1000
-#define TABSTOP 4
+#define TAB 4
 #define TRUE    1
 #define FALSE   0
 
 int getline(char line[], int maxline);
 int detab(char string[], int index);
-void entab(char line[], int start, int index);
+int entab(char line[], int start, int index);
+int parser(char line[], int start, int index);
 
 int main()
 {
@@ -26,21 +27,29 @@ int main()
 int getline(char s[], int lim)
 {
     int c, i, startofBlank, blank;
-    startofBlank = blank = 0;
+    startofBlank = 0;
+    blank = FALSE;
     
     for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
-
-        if (c == '\t' || c == ' ') {
+        printf("%d", i);
+        if ((c == '\t' || c == ' ') && blank == FALSE) {
+            printf("Enter\n");
             blank = TRUE;
             startofBlank = i;
         }
+        else if ((c == '\t' || c == ' ') && blank == TRUE)
+            ;
+            // just keep accumulating, do nothing
         else {
             if (blank == TRUE) {
                 blank = FALSE;
-                entab(s, startofBlank, i);
-            }
-            else
+                i = startofBlank + entab(s, startofBlank, i); //-1 as ++i adds 1 extra
+                printf("I after entab: %d\n", i);
                 s[i] = c;
+            }
+            else {
+                s[i] = c;
+            }
         }
         /* 
         // For 1-20
@@ -51,11 +60,22 @@ int getline(char s[], int lim)
 
         */
     }
+
+    // for the case where line is only blanks
+    if (blank == TRUE) {
+        blank = FALSE;
+        entab(s, startofBlank, i);
+    }
     if (c == '\n') {
         s[i] = c;
         ++i;
     }
     s[i] = '\0';
+
+    printf("\n\n");
+    for (int j = 0; s[j] != '\0' ; j++)
+        printf("%d ", s[j]);
+
     return i;
 }
 
@@ -77,22 +97,50 @@ int detab(char line[], int index)
 
 // Exercise 1-21
 // Start is start of blank, index is current index
-void entab(char line[], int start, int index)
+// returns new index after adding blanks
+int entab(char line[], int start, int index)
 {
-    int nextTab, extraSpaces, i;
+    printf("Start: %d End: %d\n", start, index);
+    int nextTab, extraSpaces, blanks, i;
     nextTab = extraSpaces = 0;  // set them to the starting index
     i = start;
-    while ((i + 4) < index)   // this time don't want extra tabs
-        nextTab += 4;
+    blanks = index - start;
+    printf("Num of blanks: %d\n", blanks);
+
+    while ((nextTab + TAB) <= blanks)   // this time don't want extra tabs
+        nextTab += TAB;
 
     // num of extra spaces simply rest of space if any
-    extraSpaces = index - nextTab; 
+    extraSpaces = blanks - nextTab; 
 
-    for (i = start; (start + i) < MAXLINE - 1 && i < nextTab; i += 4)
+    printf("Extra spaces needed: %d extra tabs: %d\n", extraSpaces, nextTab/4);
+
+    for (i = start; i < MAXLINE - 1 && i < start + nextTab; i += TAB) {
+        printf("Tab inserted\n");
         line[i] = '\t';
-    
-    start += i; // increment index by all the tabs
+    }
+    start = i; // increment index by all the tabs
 
-    for (i = start; (start + i) < MAXLINE - 1 && i < extraSpaces; ++i)
+    for (i = start; i < MAXLINE - 1 && i < start + extraSpaces; ++i)
         line[i] = ' ';
+
+    return (nextTab / 4 + extraSpaces);
+}
+
+// parser: given line, calculates number of spaces 
+int parser(char s[], int start, int index)
+{
+    int i, numOfBlanks;
+    numOfBlanks = 0;
+    
+    for (i = start; i < index; ++i)
+        if (s[i] == '\t') {
+            printf("tab detected\n");
+            numOfBlanks += TAB;
+        }
+        else    // would be space otherwise
+            ++numOfBlanks;
+    
+    printf("Returning: %d\n", numOfBlanks);
+    return numOfBlanks;
 }
