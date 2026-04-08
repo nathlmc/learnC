499dd6eca07055d631f2aaca788515ff738eaf15
nathlmc <nlmcfr@outlook.com>
2026-04-08 21:04:02 +0100

    Ex. 3-4, 3-5, 3-6

--- kAndR/chapter3/chapter3.c
+++ kAndR/chapter3/chapter3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 int binsearch(int x, int v[], int n); // binsearch: find x in v[0] <= v[1] <= ... <= v[n-1]
 int altbinsearch(int x, int v[], int n);    // Exercise 3-1
@@ -9,6 +10,10 @@ int atoi(char s[]);	// atoi: convert s to integer; version 2 (see chap 2)
 void shellsort(int v[], int n);	// shellsort: sort v[0]...v[n-1] into increasing order
 void reverse(char s[]);	// reverse string s in place
 void expand(char s1[], char s2[]);	// Exercise 3-3
+void itoa(int n, char s[]);	// itoa: convert n to characters in s
+void itoatwo(int n, char s[]); // Exercise 3-4
+void itob(int n, char s[], int b);	// Exercise 3-5
+void itoathree(int n, char s[], int width);	// Exercise 3-6
 
 int main()
 {
@@ -66,7 +71,7 @@ int main()
     	printf("%d ", string2[i]);
 	printf("\n");
 	*/
-
+	/*
 	// Exercise 3-3
 
 	// Normal string with no expansion
@@ -98,8 +103,43 @@ int main()
 	char string7[] = "-a-z";
 	expand(string7, string2);
 	printf("%s\n", string2);
-	
+	*/
+	/*
+	// Exercise 3-4
+	int a = INT_MIN;
+	char string1[20];
+	itoatwo(a, string1);
+	printf("%s", string1);
+	*/
+	/*
+	// Exercise 3-5
+	// Check first if same result as itoatwo
+	int a = INT_MIN;
+	char string1[20];
+	itob(a, string1, 10);
+	printf("%s\n", string1);
+	itob(a, string1, 16);
+	printf("%s\n", string1);
+	itob(a, string1, 8);
+	printf("%s\n", string1);
+	itob(a, string1, 2);
+	printf("%s\n", string1);
 
+
+	a = -378;	// Random number 
+	itob(a, string1, 16);
+	printf("%s\n", string1);
+	itob(a, string1, 8);
+	printf("%s\n", string1);
+	itob(a, string1, 2);
+	printf("%s\n", string1);
+	*/
+
+	// Exercise 3-6
+	int a = 150;
+	char string1[20];
+	itoathree(a, string1, 20);
+	printf("%s\n", string1);
     return 0;
 }
 
@@ -255,4 +295,138 @@ cases like a-b-c and a-z0-9 and -a-z. Arrange that a leading or trailing - is ta
 			s2[j] = s1[i];
 	}
 	s2[j] = '\0';
+}
+
+void itoa(int n, char s[]) // itoa: convert n to characters in s
+{
+	int i, sign;
+
+	if ((sign = n) < 0)	// record sign
+		n = -n;
+	i = 0;
+	do
+	{	// generate digits in reverse order
+		s[i++] = n % 10 + '0';	// get next digit
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
+
+void itoatwo(int n, char s[])
+// Exercise 3-4
+/*
+Exercise 3-4. In a two's complement number representation, our version of itoa does not handle the largest negative number, 
+that is, the value of n equal to -2(^(wordsize-1)). Explain why not. Modify it to print that value correctly, regardless of 
+the machine on which it runs.
+ANSWER: Since it is two's complement, the INT_MIN will hvae the property abs(INT_MIN) = abs(INT_MAX) + 1. So n = -n of itoa
+would have an overflow. We modify by dealing with the special case.
+*/
+{
+	int i, sign;
+	i = 0;
+	if ((sign = n) < 0)	{	// record sign
+		if ((n == INT_MIN))	{	//
+			s[i++] = - (n % 10) + '0';
+			n /= 10;
+		}
+		n = -n;
+	}
+	do
+	{	// generate digits in reverse order
+		s[i++] = n % 10 + '0';	// get next digit
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
+}
+
+void itob(int n, char s[], int b)
+// Exercise 3-5. 
+/*
+Write the function itob(n,s,b) that converts the integer n into a base b character representation in the
+string s. In particular, itob(n,s,16) formats n as a hexadecimal integer in s.
+*/
+{
+	int i, sign, digit;
+	// TODO: Implement for b in (10;16) -> (A;F)
+	i = 0;
+	
+
+	if ((sign = n) < 0)	{	// record sign
+		if ((n == INT_MIN))	{
+			digit = n % b;
+			if (digit < 10 || b <= 10)	{
+				s[i++] = -(digit) + '0';
+			}
+			else	{
+				s[i++] = -(digit) - 10 + 'a';
+			}
+			n /= b;
+		}
+		n = -n;
+	}
+	
+	do
+	{	// generate digits in reverse order
+		digit = n % b;
+		s[i++] = digit < 10 ? digit + '0' : digit - 10 + 'a';	// get next digit
+	} while ((n /= b) > 0);
+	
+	// End by checking cases with special notation:
+	switch (b)	{
+		case 16:
+			s[i++] = 'x';
+			s[i++] = '0';
+			break;
+		case 8:
+			s[i++] = '0';
+			break;
+		case 2:
+			s[i++] = 'b';
+			s[i++] = '0';
+			break;
+	}
+
+	if (sign < 0)
+		s[i++] = '-';
+
+
+	s[i] = '\0';
+	reverse(s);
+	
+}
+
+void itoathree(int n, char s[], int width)
+// Exercise 3-6
+/*
+Write a version of itoa that accepts three arguments instead of two. The third argument
+is a minimum field width; the converted number must be padded with blanks on the left if
+necessary to make it wide enough.
+*/
+{
+	int i, sign;
+	i = 0;
+	if ((sign = n) < 0)	{	// record sign
+		if ((n == INT_MIN))	{	//
+			s[i++] = - (n % 10) + '0';
+			n /= 10;
+		}
+		n = -n;
+	}
+	do
+	{	// generate digits in reverse order
+		s[i++] = n % 10 + '0';	// get next digit
+	} while ((n /= 10) > 0);
+	if (sign < 0)
+		s[i++] = '-';
+	// Technically, we could use itoatwo and then done int i = strlen(s);
+	while (i < width)
+		s[i++] = ' '; 
+
+	s[i] = '\0';
+
+	reverse(s);
 }
