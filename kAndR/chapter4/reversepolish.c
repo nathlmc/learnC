858eb6ad9996ee87b0e3405bf52773f18fb4ce87
nathlmc <nlmcfr@outlook.com>
2026-04-14 19:32:46 +0100

    Small bug fix for Ex 4-5. Ex 4-6

--- kAndR/chapter4/reversepolish.c
+++ kAndR/chapter4/reversepolish.c
@@ -9,6 +9,7 @@
 #define NUMBER  '0' // signal that a number was found
 #define BUFSIZE	100
 #define FUNCTION 'f'	// Exercise 4-5	
+#define VARIABLE 'A'
 
 int getch(void);
 void ungetch(int);
@@ -24,6 +25,8 @@ int sp = 0;			// next free stack position
 double val[MAXVAL];	// value stack
 char buf[BUFSIZE];	// buffer for ungetch
 int bufp = 0;		// next free position in buf
+double variables[26];	// variables. Exercise 4-6. Index would be s[0] - 'A'.
+double lastprinted = 0.0;	// store recently printed value
 
 /*
 Exercise 4-4. 
@@ -34,6 +37,14 @@ Add a command to clear the stack.
 /*
 Exercise 4-5.
 Add access to library functions like sin, exp, and pow. See <math.h> in Appendix B, Section 4.
+*/
+
+/*
+Exercise 4-6.
+Add commands for handling variables. (It's easy to provide twenty-six variables with single-letter names.)
+Add a variable for the most recently printed value.
+
+--> We treat variables as between A - Z
 */
 
 // reverse Polish calculator
@@ -57,6 +68,11 @@ int main()
 					op2 = pop();
 					push(pow(pop(), op2));
 				}
+				else if (strcmp(s, "ans") == 0)	// Exercise 4-6. Last printed value
+					push(lastprinted);
+				break;
+			case VARIABLE:	// Exercise 4-6. If user inputted a variable
+				push(variables[s[0] - 'A']);
 				break;
 			case '+':
 				push(pop() + pop());
@@ -83,8 +99,12 @@ int main()
 				else
 					printf("error: zero division\n");
 				break;
+			case '=':	// Exercise 4-6 We take this to mean variable assignment
+				variables[s[1] - 'A'] = pop();
+				break;
 			case '\n':
-				printf("\t%.8g\n", pop());
+				lastprinted = pop();
+				printf("\t%.8g\n", lastprinted);
 				break;
 			case 'p':	// peek at top element
 				peek();
@@ -150,8 +170,20 @@ int getop(char s[])	// getop: get next operator or numeric operand
 			return EOF;
 		c2 = getch();
 		if (!isalpha(c2))	{
+			if (c >= 'A' && c <= 'Z')	{	// Exercise 4-6
+				s[0] = c;
+				s[1] = '\0';
+				ungetch(c2);
+				return VARIABLE;
+			}
 			ungetch(c2);
 			return c;	// not a number, one char op
+		}
+		else if (c == '=' && (c2 >= 'A' && c2 <= 'Z'))	{	// variable assignment case
+			s[0] = c;
+			s[1] = c2;
+			s[2] = '\0';
+			return '=';
 		}
 		else	{	// Exercise 4-5
 			s[i] = tolower(c);
@@ -159,6 +191,7 @@ int getop(char s[])	// getop: get next operator or numeric operand
 			while (isalpha(c2 = getch()))
 				s[++i] = tolower(c2);
 			ungetch(c2);	// push back extra character
+			s[++i] = '\0';
 			return FUNCTION;
 		}
 	}
